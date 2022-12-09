61e82cd8ac39c9d5d7d151bcf83a5e45011f5c92
Nihal <niftynihal@gmail.com>
2022-12-09 19:57:22 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.get("/about", (req, res) => {
+    res.send("About")
+})
+
+
+app.listen(8080, () => {
+    console.log("Listening on PORT 8080")
+})
